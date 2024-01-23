4b0d6315674d815511d6dab56371c9f276bdb7ba
F2804179 Eli Sergio Bezerra De Melo Junior <eli.junior@bb.com.br>
2024-01-23 17:16:28 -0300

    feat: First Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import logoReact from "./assets/react-core-concepts.png"
+import componentImg from "./assets/components.png"
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+function CoreComponent(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function Header() {
   const description = reactDescriptions[genRandomInt(reactDescriptions.length)]
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={logoReact} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -23,7 +35,16 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Components</h2>
+          <ul>
+            <CoreComponent
+              title="Components"
+              description="The core UI building block"
+              image={componentImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
