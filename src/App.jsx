163457b706965e7996ce69c10ab0f1cc82d3da36
Eli Júnior <elijunior.py@gmail.com>
2024-01-24 02:01:01 -0300

    feat: using if statement to what to show

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { useState } from 'react';
 import componentImg from "./assets/components.png"
-import { CORE_CONCEPTS } from "./data.js"
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js"
 import Header from './components/Header/Header.jsx';
 import CoreComponent from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please select a topic');
+  const [selectedTopic, setSelectedTopic] = useState();
 
 
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton)
-
   }
+
+  let tabContent = <p>Please select a topic.</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    )
+  };
+
   return (
     <div>
       <Header />
@@ -31,11 +44,11 @@ function App() {
           <h2>Examples</h2>
           <menu>
             <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
         </section>
       </main>
     </div>
